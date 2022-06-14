4a1eafbc34cb2318f1531441baa58546eb139d95
Luis Barbosa <65256527+capitanbarbosa@users.noreply.github.com>
2022-06-14 15:18:04 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -7,28 +7,42 @@ from binance import Client
 from telegram import ParseMode
 from telegram.ext import CommandHandler, Defaults, Updater
 
-
-token = "BOT_TOKEN"  # Telegram bot token
-chat_id = "YOUR_CHAT_ID"  # Your telegram userid for bot update
+token = "5502330273:AAF43xM3FcSFwNDNwWbxgqsnagTNMw8rjAA"  # Telegram bot token
+chat_id = "1554810140"  # Your telegram userid for bot update
 
 # -100xxxxxxx add bot to your channel as admin to update on channel
-channel_id = 'YOUR CHANNEL ID'
+channel_id = '-1001775085172'
 
 fiatCurrency = 'USDT'
 
 client = Client()
 
+##
+def bruhCommand(update, context):
+    context.bot.send_message(
+        chat_id=update.effective_chat.id, text='bruh')
 
+
+def helpCommand(update, context):
+    return
+
+### wiz ^^
+
+#/start
 def startCommand(update, context):
     context.bot.send_message(
         chat_id=update.effective_chat.id, text='Hello there!, I can send you alert when your favorite crypto reaches at certain price.\nJust use command \n<i>/alert {crypto code} {> / &lt;} {price}</i>')
 
-
+#regex to format the: /alert {BTC} > {target price}
 def truncate(num):
     return re.sub(r'^(\d+\.\d{,5})\d*$', r'\1', str(num))
 
-
+#/alert
 def priceAlert(update, context):
+    # a context starts null or 0 when introduced as a param,
+    #   - When its 0, it asks for a /alert call along with a crypto symbol and its price.
+    #   - if the length of its arguments are more than 0, then updates its args each 15 sec and then
+    #       with the priceAlertCallback() and then returns a response.
     if len(context.args) > 2:
         crypto = context.args[0].upper()
         sign = context.args[1]
@@ -39,7 +53,7 @@ def priceAlert(update, context):
 
         context.job_queue.run_repeating(priceAlertCallback, interval=15, first=15, context=[
                                         crypto, sign, price, update.message.chat_id])
-
+        
         response = f"⏳ I will update you when {crypto} reaches {price} {fiatCurrency}, \n"
         response += f"the current price of {crypto} is {spotPrice} {fiatCurrency}"
     else:
@@ -47,7 +61,7 @@ def priceAlert(update, context):
 
     context.bot.send_message(chat_id=update.effective_chat.id, text=response)
     # Use this to get update on channel
-    #context.bot.send_message(channel_id, text=response)
+    context.bot.send_message(channel_id, text=response)
 
 
 def priceAlertCallback(context):
@@ -57,8 +71,10 @@ def priceAlertCallback(context):
     chat_id = context.job.context[3]
 
     send = False
+    ## Imported binance client and called get_ticker.
     result = client.get_ticker(symbol=crypto+fiatCurrency)
     d = dict()
+    # We now regex (truncate) what the result pulls from the 'lastPrice' key pair.
     spotPrice = truncate(result['lastPrice'])
 
     if sign == '<':
@@ -78,14 +94,29 @@ def priceAlertCallback(context):
         #context.bot.send_message(channel_id, text=response)
 
 
+# the update / updater is created here
+## as well as the dispatcher from it.
+###
+# We can add, edit commands from here.
+
 if __name__ == '__main__':
     updater = Updater(token=token,
                       defaults=Defaults(parse_mode=ParseMode.HTML))
     dispatcher = updater.dispatcher
+
+#/start
     dispatcher.add_handler(CommandHandler(
         'start', startCommand))
+#/alert
     dispatcher.add_handler(CommandHandler('alert', priceAlert))
-    # Remove pollibg when using webhook
+#/bruh
+    dispatcher.add_handler(CommandHandler(
+        'bruh', bruhCommand))
+
+    
+
+    ########### HOST ON HEROKU ###############
+    # - Remove polling when using webhook
     #updater.start_webhook(listen="0.0.0.0", port=os.environ.get("PORT", 443), url_path=token, webhook_url="https://appname.herokuapp.com/"+token)
     updater.start_polling()
 
